e854bf208c5416fe0e9b91f198c9189e9bec45a1
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-08-26 23:38:24 -0700

    initial commit

---
+++ roles/browser/files/user.js
@@ -0,0 +1,8 @@
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtabpage.enhanced", false);
+user_pref("browser.pocket.enabled", false);
+user_pref("general.autoScroll", true);
+user_pref("general.smoothScroll", false);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("loop.enabled", false);
